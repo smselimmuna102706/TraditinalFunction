41617da93a0a5943d67bc93f9aaf0f19455904f6
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-11 20:18:06 +0600

    TraditinalFunction

---
+++ 24.js
@@ -0,0 +1,48 @@
+
+// addition function 
+
+function addition(x,y){
+    var resutl = x+y;
+    document.write("Sum =" + resutl +"</br>");
+}
+
+addition(30,20);
+
+
+// Subtraction function 
+
+function Subtraction(x,y){
+    var resutl = x-y;
+    document.write("Sub =" + resutl +"</br>");
+}
+
+Subtraction(30,20);
+
+
+// Multiplection function 
+
+function Multiplection(x,y){
+    var resutl = x*y;
+    document.write("Mul =" + resutl +"</br>");
+}
+
+Multiplection(30,20);
+
+// Divition function 
+
+function Divition(x,y){
+    var resutl = x/y;
+    document.write("Div =" + resutl +"</br>");
+}
+
+Divition(30,20);
+
+
+// Modulas function 
+
+function Modulas(x,y){
+    var resutl = x%y;
+    document.write("Modul =" + resutl +"</br>");
+}
+
+Modulas(30,20);
